a6f259cbd0c6e84f0b22770dc553f2a50001b3ba
Sabricetin <sabriyazilim@gmail.com>
2024-09-03 18:16:57 +0300

    Initial Commit

---
+++ CryptoCrazy-Swift-UI/CryptoCrazy_Swift_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCrazy_Swift_UIApp.swift
+//  CryptoCrazy-Swift-UI
+//
+//  Created by Sabri Çetin on 3.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCrazy_Swift_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
